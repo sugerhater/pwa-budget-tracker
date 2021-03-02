a744db06616d43f6d7309bc8084de17bcdcfa16b
sugerhater <kehong0926@gmail.com>
2021-03-02 02:06:26 -0500

    add db.js to service-work.js cashe list

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,8 @@ const FILES_TO_CACHE = [
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
   '/manifest.webmanifest',
-  '/styles.css'
+  '/styles.css',
+  '/db.js'
 ];
 
 const CACHE_NAME = "static-cache-v2";
